4b24550635ab7283c09d68b74fd00d1665bf96d2
Pratik Bhuta <pratik.bhuta@blazeclan.com>
2018-08-01 18:35:05 +0530

    [STABELE]First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare function require(name: string);
+const MapboxDraw = require('@mapbox/mapbox-gl-draw');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  map;
+
+  onMapLoad(map) {
+    this.map = map;
+    const Draw = new MapboxDraw();
+    console.log(Draw);
+    console.log(this.map);
+    this.map.addControl(Draw, 'top-left');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    NgxMapboxGLModule.forRoot({
+      accessToken: 'pk.eyJ1IjoiYmxhemVjbGFuIiwiYSI6ImNqOWZxM3h1MzAzMW8zMnJxd3g0NzZ5a20ifQ.5IdwsnKmxrQftgM-3-P5IA#11.0/50.839325/4.347952/0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
