447d1b564ca64223e22351a5ce4a9477629c82b2
Pratik Bhuta <pratik.bhuta@blazeclan.com>
2018-08-02 16:47:29 +0530

    POC complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 declare function require(name: string);
-const MapboxDraw = require('@mapbox/mapbox-gl-draw');
+import mapBox = require('mapbox-gl');
+import mapBoxDraw = require('@mapbox/mapbox-gl-draw');
+import mapboxCircle = require('mapbox-gl-circle');
+// tslint:disable-next-line:max-line-length
+mapBox.accessToken = 'pk.eyJ1IjoiYmxhemVjbGFuIiwiYSI6ImNqOWZxM3h1MzAzMW8zMnJxd3g0NzZ5a20ifQ.5IdwsnKmxrQftgM-3-P5IA#11.0/50.839325/4.347952/0';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
   map;
+  coordinates: HTMLElement;
+  myCircle = new mapboxCircle({ lat: 39.984, lng: -75.343 }, 150, {
+    editable: false,
+    maxRadius: 150,
+    fillColor: '#29AB87'
+  });
+  marker = new mapBox.Marker();
+  draw = new mapBoxDraw({
+    displayControlsDefault: false,
+    controls: {
+      polygon: true,
+      trash: true
+    }
+  });
 
-  onMapLoad(map) {
-    this.map = map;
-    const Draw = new MapboxDraw();
-    console.log(Draw);
-    console.log(this.map);
-    this.map.addControl(Draw, 'top-left');
+  ngAfterViewInit() {
+    this.map = new mapBox.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v8', // stylesheet location
+      center: [-74.50, 40], // starting position
+      zoom: 9 // starting zoom
+    });
+
+    this.marker.setLngLat([-75.343, 39.984]).addTo(this.map);
+    this.map.on('click', (e) => {
+      this.marker.setLngLat([e.lngLat.lng, e.lngLat.lat]).addTo(this.map);
+      this.myCircle.setCenter({ lat: e.lngLat.lat, lng: e.lngLat.lng }).setRadius(150).addTo(this.map);
+    });
+
+    // this.map.addControl(this.draw);
   }
+
+  dragend() {
+
+  }
+
+  /*  onMapLoad(map) {
+     this.map = map;
+     const Draw = new mapBoxDraw();
+     console.log(Draw);
+     console.log(this.map);
+     this.map.addControl(Draw, 'top-left');
+   } */
 }
